a22d65309f6d1cb9ff85dfb8b11ba7709c97d6a5
Andreas Larssamils <larssamils.andreas@gmail.com>
2022-12-05 08:18:54 +0800

    added next analytics package and updated the _app.js component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
